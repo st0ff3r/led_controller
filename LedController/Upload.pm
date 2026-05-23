31d6d81b4f58a793312fa2b0da31188d50e23afb
Kristoffer Ek <stoffer@skulp.net>
2026-05-23 04:17:30 +0200

    Update Upload.pm

--- LedController/Upload.pm
+++ LedController/Upload.pm
@@ -1,94 +1,35 @@
 package LedController::Upload;
-
 use strict;
-use Data::Dumper;
-use File::Temp qw( tempfile );
-use Time::HiRes qw(usleep gettimeofday tv_interval);
 use Apache2::RequestRec;
-use Apache2::RequestIO;
-use Apache2::Const;
-use CGI;
-use CGI::Cookie ();
-
-use lib qw ( /led_controller );
+use Apache2::Const qw(:common);
+use File::Temp qw(tempfile);
+use Redis;
 use LedController;
 
-use constant REDIS_HOST => '127.0.0.1';
-use constant REDIS_PORT => '6379';
-
 sub handler {
-	my $r = shift;
-	
-	my $timestamp = int (gettimeofday * 1000);
-	my $c = new LedController;
-	
-	my %cookies = ();
-	my $session_id;
-	if (%cookies = CGI::Cookie->fetch) {
-		# cookie received
-		$session_id = $cookies{'session_id'}->value;
-		# send it again
-		my $cookie = CGI::Cookie->new(-name  => 'session_id', -value => $session_id, -samesite => 'Lax');
-		$r->err_headers_out->add('Set-Cookie' => $cookie);
-	
-		if ($c->set_session_id($session_id)) {
-			# new session created
-			$r->pool->cleanup_register(\&cleanup, {led_controller => $c, session_id => $session_id});
-	
-			my $q = new CGI(\&hook, $session_id);
-		
-			if (defined $q->param('movie_file')) {
-				my ($fh, $temp_file) = tempfile( CLEANUP => 0 );
-			
-				my $loop = $q->param('loop') || 1;
-			
-				my $buffer;
-				while (read($q->param('movie_file'), $buffer, 26214400)) {	# max 25 MB
-					print $fh $buffer;
-				}
-				close $fh;
-			
-				if ($c->movie_to_artnet(movie_file => $temp_file, artnet_data_file => "/led_controller/data/artnet.data", loop_forth_and_back => $loop)) {
-					$c->movie_to_slitscan(slitscan_file => "/var/www/led_controller/images/slitscan.png");
-				}
-			
-				unlink $temp_file;
-			}
-		}
-		else {
-			warn "session $session_id running\n";
-			
-			return Apache2::Const::FORBIDDEN;
-		}
-	}
-	return Apache2::Const::OK;
-}
+    my $r = shift;
+    my $redis = Redis->new(server => '127.0.0.1:6379');
 
-sub hook {
-	my ($filename, $buffer, $bytes_read, $id) = @_;
-	my $length = $ENV{'CONTENT_LENGTH'};
+    # Atomar lås: Hvis 'system_locked' findes, returner FORBIDDEN
+    if (!$redis->set('system_locked', '1', 'NX')) {
+        return FORBIDDEN;
+    }
 
-	my $redis_host = REDIS_HOST;
-	my $redis_port = REDIS_PORT;
-	my $redis = Redis->new(
-		server => "$redis_host:$redis_port",
-	) || warn $!;
-
-	my $progress;
-	if ($length > 0) {	# don't divide by zero.
-		$progress = sprintf("%.1f", (( $bytes_read / $length ) * 50));	# uploading accounts for 50 % of total progress
-		$redis->set('progress', $progress);
-	}
-}
-
-sub cleanup {
-	my $p = shift;
-	my $controller = $p->{led_controller};
-	my $id = $p->{session_id};
-
-	$controller->cleanup_temp_files($id);
+    my $c = LedController->new();
+    my $q = CGI->new;
+    
+    if (my $fh_in = $q->upload('movie_file')) {
+        my ($fh_out, $temp_file) = tempfile(DIR => '/tmp', SUFFIX => '.mov', UNLINK => 0);
+        while (<$fh_in>) { print $fh_out $_; }
+        close $fh_out;
+        
+        # Send job til worker
+        $redis->rpush('job_queue', $temp_file);
+        return OK;
+    }
+    
+    $redis->del('system_locked');
+    return BAD_REQUEST;
 }
 
 1;
-
-__END__
