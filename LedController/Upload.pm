0a8e1ef6482454a10a9bf81f93ab164dfedbc8ed
Kristoffer Ek <stoffer@skulp.net>
2026-05-23 04:51:05 +0200

    Update Upload.pm

--- LedController/Upload.pm
+++ LedController/Upload.pm
@@ -5,30 +5,28 @@ use Apache2::Const qw(:common);
 use File::Temp qw(tempfile);
 use Redis;
 use LedController;
+use CGI;
 
 sub handler {
     my $r = shift;
     my $redis = Redis->new(server => '127.0.0.1:6379');
 
-    # Atomar lås: Hvis 'system_locked' findes, returner FORBIDDEN
-    if (!$redis->set('system_locked', '1', 'NX')) {
+    # Check if system is already busy
+    if ($redis->exists('system_locked')) {
         return FORBIDDEN;
     }
 
-    my $c = LedController->new();
     my $q = CGI->new;
-    
     if (my $fh_in = $q->upload('movie_file')) {
         my ($fh_out, $temp_file) = tempfile(DIR => '/tmp', SUFFIX => '.mov', UNLINK => 0);
         while (<$fh_in>) { print $fh_out $_; }
         close $fh_out;
         
-        # Send job til worker
+        # Add job to queue
         $redis->rpush('job_queue', $temp_file);
         return OK;
     }
     
-    $redis->del('system_locked');
     return BAD_REQUEST;
 }
 
