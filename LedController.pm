d4458a93d369dc13c368c2db9efccb6b330e66ac
Kristoffer Ek <stoffer@skulp.net>
2026-05-26 00:18:18 +0200

    feat: add loop_forth_and_back support to optimized movie_to_artnet function

--- LedController.pm
+++ LedController.pm
@@ -43,12 +43,13 @@ sub movie_to_artnet {
 	my $self = shift;
 	my %p = @_;
 	
-	my $movie_file	   = $p{movie_file};
-	my $artnet_data_file = $p{artnet_data_file};
-	my $slitscan_file	= $p{slitscan_file};;
-	my $num_pixels	   = $config->param('num_pixels');
+	my $movie_file          = $p{movie_file};
+	my $artnet_data_file    = $p{artnet_data_file};
+	my $slitscan_file       = $p{slitscan_file};
+	my $loop_forth_and_back = $p{loop_forth_and_back} || 0;
+	my $num_pixels          = $config->param('num_pixels');
 
-	warn "[LedController] Starting nested conversion for: $movie_file\n";
+	warn "[LedController] Starting optimized conversion for: $movie_file\n";
 	$self->update_progress(50.0);
 
 	# 1. Gather metadata for progress tracking
@@ -60,11 +61,9 @@ sub movie_to_artnet {
 
 	# 2. Build the complex filter graph
 	# scale -> split into two branches: [raw] for ArtNet, [tile] for the image
-	# 2. Build the complex filter graph
 	my $filter_graph = "scale=$num_pixels:1:flags=neighbor,split[raw][tile],[tile]tile=1x${total_frames}[slitscan]";
 
 	# 3. Construct FFmpeg command
-	# We output the [raw] stream to stdout (pipe) and [slitscan] to file
 	my @cmd = (
 		'ffmpeg',
 		'-i', $movie_file,
@@ -84,23 +83,53 @@ sub movie_to_artnet {
 	open(my $pipe, "-|", @cmd) or die "FFmpeg execution failed: $!";
 	open(my $fh_out, '>', $artnet_data_file) or die "Cannot write to $artnet_data_file: $!";
 	
+	# Write the detected frame rate header
 	print $fh_out "$fps\n";
 	
 	my $row_size = $num_pixels * 3;
 	my $i = 0;
+	my @buffered_hex_frames = ();
 	
+	# Stream frames directly out of the pipeline
 	while (read($pipe, my $frame_buffer, $row_size)) {
-		print $fh_out unpack('H*', $frame_buffer) . "\n";
+		my $hex_str = unpack('H*', $frame_buffer);
+		print $fh_out "$hex_str\n";
+
+		# If looping is enabled, cache the hex lines in memory for the return trip
+		if ($loop_forth_and_back) {
+			push @buffered_hex_frames, $hex_str;
+		}
 
 		if ($i % 50 == 0) {
+			# Progress spans from 50% to roughly 99%
 			$self->update_progress(50.0 + (($i / $total_frames) * 49.0));
 		}
 		$i++;
 	}
-	
 	close($pipe);
+
+	# 5. Append the frames in reverse order if loop_forth_and_back is requested
+	if ($loop_forth_and_back && scalar(@buffered_hex_frames) >= 3) {
+		warn "[LedController] Appending reverse loop frames...\n";
+		
+		# Slit-scan animations omit the first and last frame on the bounce
+		# to ensure continuous visual playback without pause stuttering.
+		my $total_loop_steps = $#buffered_hex_frames - 1;
+		my $step = 0;
+
+		for (my $j = $#buffered_hex_frames - 1; $j >= 1; $j--) {
+			print $fh_out "$buffered_hex_frames[$j]\n";
+
+			if ($step % 50 == 0) {
+				$self->update_progress(75.0 + (($step / $total_loop_steps) * 24.0));
+			}
+			$step++;
+		}
+	}
+	
 	close($fh_out);
 	
+	$self->update_progress(100.0);
 	$self->{redis}->set('trigger_new_data', '1');
 	return 1;
 }
